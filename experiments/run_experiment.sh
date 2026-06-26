0c5004a09800932806e9aa636952754907b85020
mohammed0saeed <s.mohddheia@gmail.com>
2026-06-26 10:50:06 +0200

    updating

--- experiments/run_experiment.sh
+++ experiments/run_experiment.sh
@@ -30,33 +30,33 @@
 #    --runner.exp_name="pusht_ppo_fb_100m"
 
 ## TRIRL PPO FB
-python experiment.py \
-    --algorithm.name="trirl_ppo_fb.flax_full_jit" \
-    --algorithm.data_path="/home/kukoboshi/PycharmProjects/trust-region-irl/trirl_dataset/rl_expert/expert_dataset_Ant-v5_30_PPO.npz" \
-    --algorithm.total_timesteps=20e6 \
-    --algorithm.entropy_coef=0.001 \
-    --algorithm.clip_range=0.2 \
-    --algorithm.env_reward_frac=0.0 \
-    --algorithm.nr_steps=10 \
-    --algorithm.nr_epochs=20 \
-    --algorithm.minibatch_size=512 \
-    --algorithm.learning_rate_disc=1e-04 \
-    --algorithm.learning_rate=4e-04 \
-    --algorithm.epsilon=0.2 \
-    --algorithm.init_eta=30.0 \
-    --algorithm.gp_lambda=0.05 \
-    --algorithm.evaluation_and_save_frequency=2129920 \
-    --environment.name="ant_mjx" \
-    --environment.nr_envs=4096 \
-    --environment.seed=0 \
-    --runner.mode="train" \
-    --runner.track_tb=True \
-    --runner.track_console=True \
-    --runner.track_wandb=True \
-    --runner.save_model=True \
-    --runner.wandb_entity="s-mohddheia-tu-darmstadt" \
-    --runner.project_name="trust_region_irl" \
-    --runner.exp_name="ant_mjx_ppo_fb_50m"
+#python experiment.py \
+#    --algorithm.name="trirl_ppo_fb.flax_full_jit" \
+#    --algorithm.data_path="/home/kukoboshi/PycharmProjects/trust-region-irl/trirl_dataset/rl_expert/expert_dataset_Ant-v5_30_PPO.npz" \
+#    --algorithm.total_timesteps=20e6 \
+#    --algorithm.entropy_coef=0.001 \
+#    --algorithm.clip_range=0.2 \
+#    --algorithm.env_reward_frac=0.0 \
+#    --algorithm.nr_steps=10 \
+#    --algorithm.nr_epochs=20 \
+#    --algorithm.minibatch_size=512 \
+#    --algorithm.learning_rate_disc=1e-04 \
+#    --algorithm.learning_rate=4e-04 \
+#    --algorithm.epsilon=0.2 \
+#    --algorithm.init_eta=30.0 \
+#    --algorithm.gp_lambda=0.05 \
+#    --algorithm.evaluation_and_save_frequency=2129920 \
+#    --environment.name="ant_mjx" \
+#    --environment.nr_envs=4096 \
+#    --environment.seed=0 \
+#    --runner.mode="train" \
+#    --runner.track_tb=True \
+#    --runner.track_console=True \
+#    --runner.track_wandb=True \
+#    --runner.save_model=True \
+#    --runner.wandb_entity="s-mohddheia-tu-darmstadt" \
+#    --runner.project_name="trust_region_irl" \
+#    --runner.exp_name="ant_mjx_ppo_fb_50m"
 
 # Test Policy
 #python experiment.py \
@@ -86,10 +86,11 @@ python experiment.py \
 #
 #
 
-#python experiment.py \
-#    --algorithm.name="trirl_ppo_fb.flax_full_jit" \
-#    --environment.name="ant_mjx" \
-#    --environment.render=True \
-#    --environment.nr_envs=1 \
-#    --runner.track_tb=False --runner.track_wandb=False --runner.save_model=False
+python experiment.py \
+    --algorithm.name="trirl_ppo_fb.flax_full_jit" \
+    --environment.name="ant_mjx" \
+    --environment.render=True \
+    --environment.nr_envs=1 \
+    --runner.load_model="runs/trust_region_irl/ant_mjx_ppo_fb_50m/1782461869/models/latest.model.zip"
+    --runner.track_tb=False --runner.track_wandb=False --runner.save_model=False
 
